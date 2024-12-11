97ea1815c2bd162ae61d63f3dbc8c65160808efc
DGuzmanRomano <131337401+DGuzmanRomano@users.noreply.github.com>
2024-12-10 23:43:24 -0600

    chore: remove dist folder from tracking and add to .gitignore

--- server/dist/types/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// types/index.ts
-Object.defineProperty(exports, "__esModule", { value: true });
--- server/dist/utils/regex.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.badAbbreviationsRegex = exports.specialCharsRegex = exports.repeatedDashesRegex = exports.missingSpaceRegex = void 0;
-exports.missingSpaceRegex = /\b\w+,(?=[a-zA-Z])/g;
-exports.repeatedDashesRegex = /--|- -/g;
-exports.specialCharsRegex = /[^a-zA-Z0-9\s.,;:()\-]/g;
-exports.badAbbreviationsRegex = /\b(pck|pkg|qty|qt|pc|pcs|ea|(?<=\s|^)in\.(?=\s|$)|ft)\b/gi;
